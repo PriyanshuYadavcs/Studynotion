39eaaf3863098312aaa3207f76f527d290f28902
officialpriyanshu22 <156700602+PriyanshuYadavcs@users.noreply.github.com>
2024-09-05 17:41:20 +0530

    initialcommit

---
+++ server/models/CourseProgress.js
@@ -0,0 +1,19 @@
+const mongoose= require("mongoose");
+const courseProgress= new mongoose.Schema({
+    courseId: {
+        type:mongoose.Schema.Types.ObjectId, 
+        ref:"Course", 
+    }, 
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "user", 
+    }, 
+    completedVideos: [
+        {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref:"Subsection", 
+        }
+    ]
+})
+
+module.exports= mongoose.model("CourseProfess", courseProgress); 
